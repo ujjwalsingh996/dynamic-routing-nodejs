536f52c86ceaf58c292f6ad61fd7887bf8d338b4
ujjwalsingh996 <ujjwalsingh996@gmail.com>
2023-04-23 22:25:41 +0530

    changes through seqeulize

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 
-const db = require("./util/database");
+const sequelize = require("./util/database");
 
 const app = express();
 
@@ -24,4 +24,14 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize.sync().then(result => {
+    // console.log(result)
+    app.listen(3000);
+}).catch(err => {
+    console.log(err)
+})
+
+
+
+
+
